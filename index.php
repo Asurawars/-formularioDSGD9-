129e8bba58d9100adf95f04061cd0c289b508369
Asurawars <gonzalezdember2001@gmail.com>
2020-03-28 20:56:18 -0400

    mi primer commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<ul>
+		<li><a href="Formulario.html">Formulario</li>
+		<li><a href="#">Modulo 1</li>
+		<li><a href="#">Modulo 2</li>
+	</ul>
+</body>
+</html>
