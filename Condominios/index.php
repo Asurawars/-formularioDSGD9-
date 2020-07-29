b5333b4f111fe01dae586c69303d5e62b7ef4cda
Asurawars <dembergonzalez2001@gmail.com>
2020-07-29 14:00:46 -0400

    mi primer commit

--- Condominios/index.php
+++ Condominios/index.php
@@ -1,56 +1,64 @@
 <?php
-include("clasedb2.php");
+include('clasedb2.php');
 extract($_REQUEST);
 $db=new clasedb2();
 $conex=$db->conectar();
 
-$registrar= 20; // numero a registrar
-$estacionamiento = array('Si', 'No');
-$status = array('Ocupado', 'Desocupado', 'Mantenimiento');
-$tipo = array('Casa', 'Apartamento', 'Chalet', 'Quinta', 'Otro');
-$postal = array('2121', '2103', '2122');
-
-$mds = 0;
-$err = array();
-$agregar = array();
-$o = 0;
-
-for ($i=1; $i <= 20; $i++) { 
-	$irand = rand(0,1000);
-	$erand = rand(0, count($estacionamiento) -1);
-	$srand = rand(0, count($status) -1);
-	$trand = rand(0, count($tipo) -1);
-	$prand = rand(0, count($postal) -1);
-
-$sql = "INSERT INTO inmuebles (idem, estacionamiento, status, tipo, cod_postal) VALUES ('".$irand."','".$estacionamiento[$erand]."','".$status[$srand]."','".$tipo[$trand]."','".$postal[$prand]."')";
-
- if ($conex->query($sql)) {
-	$agregar[$mds] = $irand;
-	$mds++;
-}else{
-	$error[$o] = $conex->error;
-	$e++;
-  }
- }
-
-echo "<br/>";
-echo "Registro fallido: 0 <br/><br/>";
-for ($i=0; $i < count($err); $i++) { 
-	echo "Si: ".$err[$i]."<br/>";
-}
-
-echo "Registro añadido: $mds <br/><br/>";
-for ($i=0; $i < count($agregar); $i++) { 
-	echo "Si: Apartamento".$agregar[$i]."<br/>";
- }
-/*
-$sql="CREATE TABLE inmuebles (id INT(11) NOT NULL PRYMARY KEY AUTO_INCREMENT, idem VARCHAR(50) NOT NULL, estacioamiento ENUM('Si','No'), status ENUM('Ocupado', 'Desocupado', 'Mantenimiento'), tipo('Casa', 'Apartamento', 'Chalet', 'Quinta', 'Otro'), cod_postal VARCHAR(5));";
+$sql="CREATE TABLE inmuebles (id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, idem varchar(50) NOT NULL, estacionamiento enum('Si','No'), status enum('Ocupado','Desocupado','Mantenimiento'), tipo enum('Casa','Apartamento','Chalet','Quinta','Otro'), cod_postal varchar(5))";
 
 $result=mysqli_query($conex,$sql);
-if ($result) {
-	echo "La Tabla Creada Exitosamente";
-}else{
-	echo "Tabla No Creada";
+
+if($result)
+{
+	echo 'Tabla Creada';
+	$sql="ALTER TABLE inmuebles ADD UNIQUE (idem)";
+	$result=mysqli_query($conex,$sql);
+	if($result)
+	{
+		echo ' Se asingo un atributo UNIQUE al idem';
+	}else{
+		echo ' No logro asingar el atributo UNIQUE al idem';
+	}
 }
-*/
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tabla de inmuebles</title>
+</head>
+<body>
+	<button><a href="rmd.php">Ingresar Registros</a></button>
+	<br/><br/>
+	<button><a href="obtener_datos_inmuebles.php">Ver Registros</a></button>
+	<table align="center">
+		<tr align="center">
+			<th colspan="6" bgcolor="#E30F87">Registros</th>
+		</tr>
+		<tr align="center">
+			<td>ID</td>
+			<td>Identificacion</td>
+			<td>Estacionamiento</td>
+			<td>Estatus</td>
+			<td>Tipo</td>
+			<td>Codigo Postal</td>
+		</tr>
+		<?php 
+ 			$sql="SELECT * FROM inmuebles";
+ 			$res=mysqli_query($conex,$sql);
+ 			while($reg=mysqli_fetch_array($res))
+ 			{
+ 		?>
+ 		<tr align="center">
+ 		<td><?php echo $reg['id']; ?></td>
+ 		<td><?php echo $reg['idem']; ?></td>
+ 		<td><?php echo $reg['estacionamiento']; ?></td>
+ 		<td><?php echo $reg['status']; ?></td>
+ 		<td><?php echo $reg['tipo']; ?></td>
+ 		<td><?php echo $reg['cod_postal']; ?></td>
+ 		</tr>
+ 		<?php } ?>
+ 		</tr>
+	</table>
+
+</body>
+</html>
